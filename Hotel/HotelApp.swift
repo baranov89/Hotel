8e24e66eb95c50d593d7263705ce57ee42643da5
aleksey <a.baranov@vsa.su>
2023-09-06 11:25:16 +0500

    Initial Commit

---
+++ Hotel/HotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelApp.swift
+//  Hotel
+//
+//  Created by Алексей Баранов on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
